c15cad15d8990d3a86e3b620851fb13b7e7a6eb4
38068 <38068@DESKTOP-SRPFKQ2>
2020-11-22 23:50:24 +0200

    first commit

--- public_html/src/intens.service.js
+++ public_html/src/intens.service.js
@@ -53,7 +53,7 @@
             });
         };
         service.getExperts = function () {
-           
+
             return $http.get(ApiPath + '/experts.json').then(function (response) {
                 return response.data;
 
@@ -61,12 +61,9 @@
         };
 
         service.getPerson = function (id) {
-            console.log('+++');
             return $http.get(ApiPath + '/founders.json').then(function (response) {
                 var person;
-                console.log('yes');
                 person = response.data.find(record => record.id === id);
-                console.log(person);
                 return person;
             });
         };
